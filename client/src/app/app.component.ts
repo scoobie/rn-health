294ca45c87b127ba181a9dbc504f3e438925b7d7
scoobie <rolando.a@sapo.pt>
2021-09-10 15:39:12 +0100

    angular done with https

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+  users:any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(){
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(data=>{
+      this.users=data;
+    },error=>{
+      console.log(error);
+    });
+  }
+
+
 }
